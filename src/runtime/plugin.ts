d0e50e116df56ba81a84e7fde1887f9853d8f9e1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-05-02 01:43:13 -0300

    feat: improve plugin

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -2,10 +2,27 @@ import { defineNuxtPlugin } from '#app'
 import dayjs from 'dayjs'
 import '#build/dayjs.config.mjs'
 
-export default defineNuxtPlugin(() => {
-  return {
-    provide: {
-      dayjs
-    }
-  }
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide('dayjs', dayjs)
 })
+
+interface PluginInjection {
+  $dayjs: typeof dayjs
+}
+
+// Nuxt Bridge & Nuxt 3
+declare module '#app' {
+  interface NuxtApp extends PluginInjection {
+  }
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties extends PluginInjection {
+  }
+}
+
+// @ts-ignore
+declare module 'vue/types/vue' {
+  interface Vue extends PluginInjection {
+  }
+}

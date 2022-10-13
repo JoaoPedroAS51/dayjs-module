8b49444c287db78097e663bb0c48a8c796ebb74d
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-10-13 13:49:12 -0300

    fix: fix dayjs import and refactor plugin

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,28 +1,11 @@
-import { defineNuxtPlugin } from '#app'
-import dayjs from 'dayjs/esm/index'
+import { defineNuxtPlugin } from '#imports'
+import dayjs from 'dayjs'
 import '#build/dayjs.config.mjs'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.provide('dayjs', dayjs)
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      dayjs
+    }
+  }
 })
-
-interface PluginInjection {
-  $dayjs: typeof dayjs
-}
-
-// Nuxt Bridge & Nuxt 3
-declare module '#app' {
-  interface NuxtApp extends PluginInjection {
-  }
-}
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties extends PluginInjection {
-  }
-}
-
-// @ts-ignore
-declare module 'vue/types/vue' {
-  interface Vue extends PluginInjection {
-  }
-}
